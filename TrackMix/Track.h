62974405f09f63a67101d067b05465712f1c68d2
Jasmine <jasmine.chan89@gmail.com>
2015-10-15 22:53:02 -0400

    first commit

---
+++ TrackMix/Track.h
@@ -0,0 +1,13 @@
+//
+//  Track.h
+//  TrackMix
+//
+//  Created by JasmineChan on 2015-10-10.
+//  Copyright (c) 2015 chanizzle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Track : NSObject
+
+@end
---
+++ TrackMix/Track.m
@@ -0,0 +1,13 @@
+//
+//  Track.m
+//  TrackMix
+//
+//  Created by JasmineChan on 2015-10-10.
+//  Copyright (c) 2015 chanizzle. All rights reserved.
+//
+
+#import "Track.h"
+
+@implementation Track
+
+@end
